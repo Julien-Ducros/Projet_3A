8fc4e6308e9e63c5f2b5c412e58fe6b81e6131f0
Julien <ducrosjulien.pro@gmail.com>
2020-05-08 15:49:23 +0200

    Ajout de la classe java de la page_2 et modification du main pour aller de la première page a la deuxième avec le bouton "enter" et pouvoir revenir sur la première page avec la flèche en haut a gauche

---
+++ app/src/main/java/Page_2.java
@@ -0,0 +1,14 @@
+package com.example.projet_progmobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication_projet.R;
+
+public class Page_2 extends AppCompatActivity {
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_2);
+    }
+}
--- app/src/main/java/com/example/myapplication_projet/MainActivity.java
+++ app/src/main/java/com/example/myapplication_projet/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.myapplication_projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // showlist();
+        // makeApiCall();
+    }
+    public void page_1 (View view) {
+        startActivity(new Intent(this, com.example.projet_progmobile.Page_2.class));
     }
 }
